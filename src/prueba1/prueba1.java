620df2889d562f8ffdd38036e388fc6945de4d56
jairo6489 <jairo6489@gmail.com>
2018-11-16 09:59:09 +0100

    commit1
    creación de la clase prueba

---
+++ src/prueba1/prueba1.java
@@ -0,0 +1,15 @@
+package prueba1;
+
+public class prueba1 {
+
+	public static void main(String[] args) {
+		int i = 1;
+		boolean exit = false;
+
+		if (i >= 1) {
+			System.out.println(exit);
+		}
+
+	}
+
+}
